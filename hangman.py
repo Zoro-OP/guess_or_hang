e3ed59320cf3c29b94600edea963c6630c2f54b4
Md. Shoieb Hossain <63445638+Zoro-OP@users.noreply.github.com>
2022-06-12 12:45:37 +0600

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,3 +1,56 @@
+import random
+import time
+
+# welcome message:
+print("\nWelcome to DIE!!!\n")
+name = input("Enter your name: ")
+print("Hello " + name + "! Best of Luck!")
+time.sleep(2)
+print("The game is about to start!\n Ready to Hang Kids!!!")
+time.sleep(3)
+
+def main():
+    global count
+    global display
+    global word
+    global already_guessed
+    global length
+    global play_game
+    words_to_guess = ["january","border","image","film","promise","kids","lungs","doll","rhyme","damage","plants"]
+    word = random.choice(words_to_guess)
+    length = len(word)
+    count = 0
+    display = '_' * length
+    already_guessed = []
+    play_game = ""
+
+# re-execute the game when the first round ends:
+
+def play_loop():
+    global play_game
+    play_game = input("Do You want to play again? y = yes, n = no \n")
+    while play_game not in ["y", "n","Y","N"]:
+        play_game = input("Do You want to play again? y = yes, n = no \n")
+    if play_game == "y":
+        main()
+    elif play_game == "n":
+        print("Thanks For Playing! We expect you back again!")
+        exit()
+    
+# required conditions:
+def hangman():
+    global count
+    global display
+    global word
+    global already_guessed
+    global play_game
+    limit = 5
+    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
+        print("Invalid Input, Try a letter\n")
+        hangman()
+
     elif guess in word:
         already_guessed.extend([guess])
         index = word.find(guess)
@@ -70,7 +123,7 @@
                   "  |    / \ \n"
                   "__|__\n")
             print("Wrong guess. You are hanged!!!\n")
-            print("The word was:",already_guessed,word)
+            print("The word was:", already_guessed, word)
             play_loop()
 
     if word == '_' * length:
